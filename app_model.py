e542e77e06f5c638c32dcc7e273d4412b52ca0be
JaredR33 <118567250+JaredR33@users.noreply.github.com>
2023-02-28 20:10:17 +0100

    desplegar en cloud

--- app_model.py
+++ app_model.py
@@ -3,6 +3,8 @@ import os
 import pickle
 from sklearn.model_selection import cross_val_score
 import pandas as pd
+import sqlite3
+import os
 
 
 os.chdir(os.path.dirname(__file__))
@@ -28,5 +30,43 @@ def predict():
     else:
         prediction = model.predict([[tv,radio,newspaper]])
         return "The prediction of sales investing that amount of money in TV, radio and newspaper is: " + str(round(prediction[0],2)) + 'k €'
+
+#2 Un endpoint para almacenar nuevos registros en la base de datos que deberá estar previamente creada.(/v2/ingest_data)
+
+@app.route('/v2/ingest_data', methods=['POST'])
+def ingest_data():
+    # Obtener los datos de la solicitud
+    tv = request.args.get('tv', None)
+    radio = request.args.get('radio', None)
+    newspaper = request.args.get('newspaper', None)
+    sales = request.args.get('sales', None)
+
+
+    # Establecer una conexión con la base de datos y crear un cursor
+    conn = sqlite3.connect('advertising2.db')
+    cursor = conn.cursor()
+    
+    cursor.execute('''CREATE TABLE IF NOT EXISTS campanias
+                  (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   TV REAL,
+                   radio REAL,
+                   newspaper REAL,
+                   sales REAL);''')
+    
+    # Insertar los datos en la tabla "campanias"
+    cursor.execute('INSERT INTO campanias (TV, radio, newspaper, sales) VALUES (?, ?, ?, ?)',
+                   (tv, radio, newspaper, sales))
+    
+    # Confirmar los cambios en la base de datos y cerrar la conexión
+    conn.commit()
+    
+    # Devolver una respuesta indicando que los datos se han insertado correctamente
+    return 'Los datos se han insertado correctamente.'
+
+
+#3 
+
+
+
 
 app.run()
